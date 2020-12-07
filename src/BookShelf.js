c4426e85322494d6511ee93581ab7296eb51430d
Oliver Maerz <7649813+olivermaerz@users.noreply.github.com>
2020-12-07 20:25:56 -0300

    Add basic components for bookshelf and book

---
+++ src/BookShelf.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class BookShelf extends Component {
+    render() {
+        return (
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">Currently Reading</h2>
+                <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        <li>
+
+                        </li>
+                    </ol>
+                </div>
+            </div>
+        )
+    }
+}
